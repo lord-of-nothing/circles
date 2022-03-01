7b76748b5898f313bde59d59239c42b657fba2ca
Edward <thebestoneslie@ya.ru>
2022-03-01 23:04:56 +0300

    Решена задача "Git и случайные окружности"

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import sys
 from random import randint
-from PyQt5 import uic
+from UI import Ui_MainWindow
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
-class MainW(QMainWindow):
+class MainW(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.w = self.width()
         self.h = self.height()
         self.button.clicked.connect(self.paint)
@@ -28,13 +28,14 @@ class MainW(QMainWindow):
         self.repaint()
 
     def draw_random_circle(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
-        qp.setPen(QColor(255, 255, 0))
+        color = QColor(randint(0, 256), randint(0, 256), randint(0, 256))
         x = randint(0, self.w - 100)
         y = randint(0, self.h - self.button.height() - 100)
         d = randint(10, 100)
-        self.circles.append((x, y, d))
+        self.circles.append((x, y, d, color))
         for c in self.circles:
+            qp.setBrush(c[3])
+            qp.setPen(c[3])
             qp.drawEllipse(c[0], c[1], c[2], c[2])
 
 
